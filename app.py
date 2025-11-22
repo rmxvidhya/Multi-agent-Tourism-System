81b99ce28a0889acc28dfac0831677a940cbb056
rmxvidhya <rmvidhya279@gmail.com>
2025-11-22 18:51:33 +0530

    hotfix2

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def health():
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
+
+application = app
